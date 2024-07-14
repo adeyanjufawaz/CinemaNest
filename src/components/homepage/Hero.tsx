9c6ba9e26627e00506d5071f4f4ee2fe7d7d376a
Adeyanju Fawaz <adeyanjufawaz999@gmail.com>
2024-07-14 10:37:48 +0100

    2nd Commit

--- src/components/homepage/Hero.tsx
+++ src/components/homepage/Hero.tsx
@@ -1,21 +1,27 @@
 import img1 from "../../img/homeImg1.png";
 import img2 from "../../img/homeImg2.png";
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 function Hero() {
   return (
     <div className="grid grid-cols-1 py-6 lg:grid-cols-2 gap-5 justify-between h-auto ">
       <div className="flex justify-center items-center p-4 lg:p-10 ">
         <div className="div relative  w-[120px] lg:w-[200px]">
-          <img
-            src={img1}
+          <LazyLoadImage
             alt="movie image"
-            className="w-full"
+            height={300}
+            src={img2} 
+            
+            width={200}
           />
-          <img
-            src={img2}
+          <LazyLoadImage
             alt="movie image"
-            className="w-full absolute top-[20%] -right-[30%] "
+            height={300}
+            src={img1} 
+            
+            width={200}
           />
+          
         </div>
       </div>
       <div className="flex flex-col justify-center items-start p-7">
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -36,14 +36,14 @@ function Navbar() {
     <nav className="navbar">
       <Link to="/" className="logo">CinemaNest</Link>
       <div className="searchBar">
-        <input type="text" onChange={(e) => setInput(e.target.value)} onClick={() => setHideSearch(false)} />
+        <input type="text" value={input} onChange={(e) => setInput(e.target.value)} onClick={() => setHideSearch(false)} />
         <CiSearch className="searchIcon" />
         <div
           className="absolute right-[2.5%] top-[110%] w-[95%] bg-white shadow-md shadow-red rounded-md lg:max-h-64 max-h-48 p-2 flex flex-col scroll-auto overflow-y-auto gap-2"
           style={hideSearch ? { display: "none" } : { display: "flex" }}
         >
           {suggestionLists.map((list:{id:string,title:string}) => (
-            <Link to={`movie/${list.title}/${list.id}`} key={list.id} className="border-b-4 cursor-pointer p-3 rounded" onClick={()=>{setHideSearch(true)}}>{list.title}</Link>
+            <Link to={`movie/${list.title}/${list.id}`} key={list.id} className="border-b-4 cursor-pointer p-3 rounded" onClick={() => { setHideSearch(true); setInput("")}}>{list.title}</Link>
           ))}
 
         </div>
